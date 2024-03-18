b3b1ba7ffd7a97c775ff6445b9580b66a23d0b4a
Sergi Mula <sergiserch@gmail.com>
2024-03-19 00:42:22 +0100

    fix: escape characters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center flex-col text-center">
         <h2 className="text-2xl font-bold text-carrot mb-3">
-          Hello, I'm Sergi Mulà!
+          Hello, I&apos;m Sergi Mulà!
         </h2>
         <p className="text-l text-light-chocolate flex place-items-center flex-col">
-          I'm a Senior Web Engineer based in Barcelona.
+          I&apos;m a Senior Web Engineer based in Barcelona.
           <br />
           My passions are web development, and vintage style motorcycles.
         </p>
         <MotorcycleIcon />
         <p className="text-l text-light-chocolate">
-          I'm currently working at <a className="underline" href="https://n26.com">N26</a>
+          I&apos;m currently working at <a className="underline" href="https://n26.com">N26</a>
         </p>
         <ul className="flex space-x-2 mt-2 place-items-center">
           <li>
